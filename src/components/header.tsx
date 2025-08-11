520e016607463a6b6eb97da9e2ed2caa161c40d7
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-08-11 11:23:20 +0200

    started with the header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,42 @@
+import { Leaf } from "lucide-react";
+import Link from "next/link";
+
 export default function Header() {
   return (
     <nav>
-      <div></div>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center space-x-8">
+          <Leaf className="h-8 w-8 text-emerald-600" />
+          <span>Plant guide</span>
+        </div>
+
+        <div>
+          <Link
+            href={"/"}
+            className="text-gray-200 hover:text-emerald-400 transition-colors"
+          >
+            Home
+          </Link>
+          <Link
+            href={"/guides"}
+            className="text-gray-200 hover:text-emerald-400 transition-colors"
+          >
+            Guides
+          </Link>
+          <Link
+            href={"/Tools"}
+            className="text-gray-200 hover:text-emerald-400 transition-colors"
+          >
+            Tools
+          </Link>
+          <Link
+            href={"/about-us"}
+            className="text-gray-200 hover:text-emerald-400 transition-colors"
+          >
+            About us
+          </Link>
+        </div>
+      </div>
     </nav>
   );
 }
