a508239759f8eefa17e606c0ec464a5bea1a934b
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-08-11 16:21:08 +0200

    feat: add database schema and configuration
    - Added dotenv for environment variable management.
    - Introduced drizzle-orm for database interactions.
    - Created a new schema for plants with various attributes and enums for care level, light requirement, and humidity preference.
    - Initialized database connection in index.ts.
    - Updated package.json to include new dependencies: dotenv, drizzle-orm, pg, @types/pg, drizzle-kit, and tsx.

---
+++ src/app/guides/schema.ts
@@ -0,0 +1,35 @@
+import {
+  integer,
+  pgEnum,
+  pgTable,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const careLevelEnum = pgEnum("care_level", ["easy", "medium", "hard"]);
+export const lightRequirementEnum = pgEnum("light_requirement", [
+  "low",
+  "medium",
+  "bright",
+  "direct",
+]);
+export const humidityPreferenceEnum = pgEnum("humidity_preference", [
+  "easy",
+  "medium",
+  "hard",
+]);
+
+export const plantTable = pgTable("plants", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  scientific_name: varchar({ length: 255 }).notNull(),
+  description: varchar({ length: 255 }).notNull(),
+  water_frequency_days: integer().notNull(),
+  temperature_min: integer().notNull(),
+  temperature_max: integer().notNull(),
+  image_url: varchar({ length: 255 }).notNull(),
+  created_at: timestamp("timestamp1").notNull().defaultNow(),
+  care_level: careLevelEnum().notNull(),
+  light_requirement: lightRequirementEnum().notNull(),
+  humidity_preference: humidityPreferenceEnum().notNull(),
+});
