81e337f5ae13d11b1027f7aadb39c040450047f0
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-08-13 11:34:07 +0200

    add AdminDashboard component and page

---
+++ src/app/admin-dashboard/page.tsx
@@ -0,0 +1,9 @@
+import AdminDashboard from "@/features/admin-dashboard/components/admin-dashboard";
+
+export default function Page() {
+  return (
+    <div>
+      <AdminDashboard />
+    </div>
+  );
+}
