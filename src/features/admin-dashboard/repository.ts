3b722fd3edb52f998cd73caf039f0c8ea207781b
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-09-16 11:36:20 +0200

    ref: removed unused functions

--- src/features/admin-dashboard/repository.ts
+++ src/features/admin-dashboard/repository.ts
@@ -8,36 +8,6 @@ import { ArticleStatusType } from "../articles/types";
 
 export default function createAdminDashboardRepository(db: Db) {
   return {
-    async addPlant(newPlant: NewPlant) {
-      try {
-        const result = await db
-          .insert(plantTable)
-          .values(newPlant)
-          .returning({ id: plantTable.id });
-        if (result.length > 0) {
-          return { success: true, message: "Plant inserted successfully" };
-        }
-        return {
-          success: false,
-          message:
-            "There was a problem with adding the plant to the database, please try again.",
-        };
-      } catch (error) {
-        if ((error as { code: string }).code === "23505") {
-          return {
-            success: false,
-            message: "plant already registered.",
-            error,
-          };
-        }
-        return {
-          success: false,
-          message:
-            "There was a problem with adding the plant to the database, please try again.",
-          error,
-        };
-      }
-    },
     async addArticle(newArticle: NewArticle) {
       try {
         const result = await db
@@ -68,17 +38,11 @@ export default function createAdminDashboardRepository(db: Db) {
         };
       }
     },
-    async getAllPlants(): Promise<Plants[]> {
-      const plants = await db.select().from(plantTable);
-      return plants;
-    },
     async getAllArticles() {
       const articles = await db.select().from(articleTable);
       return articles;
     },
-    async deletePlant(PlantId: number) {
-      await db.delete(plantTable).where(eq(plantTable.id, PlantId));
-    },
+
     async deleteArticle(articleId: number) {
       await db.delete(articleTable).where(eq(articleTable.id, articleId));
     },
