1f1e8b814682ef7145f462bc1a04e07b9dca8512
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-09-16 15:11:11 +0200

    created userRole table in iamFeature

--- src/features/iam/schema.ts
+++ src/features/iam/schema.ts
@@ -9,3 +9,17 @@ export const users = pgTable("users", {
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
+
+export const roles = pgTable("roles", {
+  id: text("id").primaryKey(),
+  description: text("description"),
+});
+
+export const userRoles = pgTable("user_roles", {
+  userId: text("user_id")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  roleId: text("role_id")
+    .notNull()
+    .references(() => roles.id, { onDelete: "cascade" }),
+});
