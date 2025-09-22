580bb8d1404a101b612547edb44f1aec4d7f9a7c
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-09-22 10:35:09 +0200

    removed old imports

--- src/features/admin-dashboard/repository.ts
+++ src/features/admin-dashboard/repository.ts
@@ -1,10 +1,4 @@
-import { eq } from "drizzle-orm";
 import { Db } from "../../db/index";
-import { plantTable } from "../plant-guides/schema";
-import { Plants, NewPlant, NewArticle } from "./types";
-
-import { articleStatusEnum, articleTable } from "../articles/schema";
-import { ArticleStatusType } from "../articles/types";
 
 export default function createAdminDashboardRepository(db: Db) {
   return {};
