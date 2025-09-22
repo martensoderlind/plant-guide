8784839142aab0275f271caeabcbee69dc7f005f
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-09-22 10:33:18 +0200

    added missing key prop

--- src/features/admin-dashboard/components/admin-dashboard-fallback.tsx
+++ src/features/admin-dashboard/components/admin-dashboard-fallback.tsx
@@ -23,7 +23,10 @@ export default function AdminDashboardFallback({
           <thead className="bg-gray-50">
             <tr>
               {tableHeaders.map((tableHeader, index) => (
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                <th
+                  key={index}
+                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
                   {tableHeaders[index]}
                 </th>
               ))}
