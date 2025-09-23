a64393d928e93cd89f2ec2edc859b46301199927
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-09-23 16:24:36 +0200

    added pagination to article

--- src/app/admin-dashboard/articles/page.tsx
+++ src/app/admin-dashboard/articles/page.tsx
@@ -1,4 +1,5 @@
 import AdminArticles from "@/features/admin-dashboard/components/admin-articles/admin-articles";
+import { adminDashboardService } from "@/features/admin-dashboard/instance";
 type Props = {
   searchParams: Promise<{ page?: string }>;
 };
@@ -6,11 +7,11 @@ type Props = {
 export default async function Page({ searchParams }: Props) {
   const params = await searchParams;
   const currentPage = Number(params?.page) || 1;
-
+  const articleCount = await adminDashboardService.getArticleCount();
   return (
     <div className="min-h-screen">
       <div>
-        <AdminArticles currentPage={currentPage} />
+        <AdminArticles currentPage={currentPage} articleCount={articleCount} />
       </div>
     </div>
   );
--- src/features/admin-dashboard/components/admin-articles/admin-articles.tsx
+++ src/features/admin-dashboard/components/admin-articles/admin-articles.tsx
@@ -2,12 +2,21 @@ import { Suspense } from "react";
 import AdminArticleContainer from "./admin-article-container";
 import AdminArticleForm from "./admin-article-form";
 import AdminDashboardFallback from "../admin-dashboard-fallback";
+import Pagination from "@/components/pagination";
 
 type Props = {
   currentPage: number;
+  articleCount: number;
 };
 
-export default function AdminArticles({ currentPage }: Props) {
+export default function AdminArticles({ currentPage, articleCount }: Props) {
+  const totalPages = (totalPlants: number) => {
+    if (totalPlants % 6 === 0) {
+      return totalPlants / 6;
+    } else {
+      return totalPlants / 6 + 1;
+    }
+  };
   return (
     <div className="space-y-6">
       <AdminArticleForm />
@@ -29,6 +38,7 @@ export default function AdminArticles({ currentPage }: Props) {
       >
         <AdminArticleContainer currentPage={currentPage} />
       </Suspense>
+      <Pagination totalPages={totalPages(articleCount)} />
     </div>
   );
 }
