3f1a3da68d6797b6bea0edb6a507e7e24ebd071a
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-09-24 20:26:32 +0200

    new image path for next images

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+      },
     ],
     formats: ["image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200],
