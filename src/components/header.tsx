b1e483a902e7c8accb0f55e298a77803e791b2fb
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-09-24 14:43:15 +0200

    added clerk components to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,11 +3,20 @@ import { usePathname } from "next/navigation";
 import { Leaf, Menu, X } from "lucide-react";
 import Link from "next/link";
 import { useState } from "react";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  useAuth,
+  UserButton,
+} from "@clerk/nextjs";
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const pathname = usePathname();
   const isActive = (path: string) => pathname === path;
+  const { isSignedIn } = useAuth();
   return (
     <nav>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -61,57 +70,39 @@ export default function Header() {
             >
               About us
             </Link>
-            <button className="bg-emerald-600 text-white px-6 py-2 rounded-full hover:bg-emerald-700 transition-colors">
-              Start Today
-            </button>
-          </div>
-          <button
-            className="md:hidden"
-            onClick={() => setIsMenuOpen(!isMenuOpen)}
-          >
-            {isMenuOpen ? (
-              <X className="h-6 w-6" />
+            {isSignedIn ? (
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
             ) : (
-              <Menu className="h-6 w-6" />
+              <button
+                className="bg-emerald-600 text-white px-6 py-2 rounded-full hover:bg-emerald-700 transition-colors"
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+              >
+                {isMenuOpen ? (
+                  <X className="h-6 w-6" />
+                ) : (
+                  <span>Start Today</span>
+                )}
+              </button>
             )}
-          </button>
+          </div>
         </div>
       </div>
       {isMenuOpen && (
-        <div className="md:hidden bg-gray-700">
-          <div className="flex flex-col px-4 py-4 space-y-4">
-            <Link
-              href={"/"}
-              className="text-gray-200 hover:text-emerald-400 transition-colors"
-            >
-              Home
-            </Link>
-            <Link
-              href={"/plant-guides"}
-              className="text-gray-200 hover:text-emerald-400 transition-colors"
-            ></Link>
-            <Link
-              href={"/articles"}
-              className="text-gray-200 hover:text-emerald-400 transition-colors"
-            >
-              Articles
-            </Link>
-            <Link
-              href={"/tools"}
-              className="text-gray-200 hover:text-emerald-400 transition-colors"
-            >
-              Tools
-            </Link>
-            <Link
-              href={"/about-us"}
-              className="text-gray-200 hover:text-emerald-400 transition-colors"
-            >
-              About us
-            </Link>
-            <button className="bg-emerald-600 text-white px-6 py-2 rounded-full hover:bg-emerald-700 transition-colors">
-              Start Today
-            </button>
-          </div>
+        <div className="flex flex-col pt-2 w-1/10 mx-auto">
+          <SignedOut>
+            <SignInButton>
+              <button className="my-2 bg-emerald-600 hover:bg-emerald-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                <span>Sign in</span>
+              </button>
+            </SignInButton>
+            <SignUpButton>
+              <button className="bg-emerald-600 hover:bg-emerald-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                Sign Up
+              </button>
+            </SignUpButton>
+          </SignedOut>
         </div>
       )}
     </nav>
