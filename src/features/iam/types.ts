c3cc9f758d286b4b921536712d48e4d528a7a1a0
mårten söderlind <marten.soderlind@gmail.com>
2025-11-05 11:17:31 +0100

    new types for permissions

--- src/features/iam/types.ts
+++ src/features/iam/types.ts
@@ -1,3 +1,4 @@
+import { rolesPermissions } from "./permissions";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type Service = { [key: string]: (args: any) => Promise<any> };
 
@@ -7,3 +8,7 @@ export type PermissionSchema<
 > = {
   [k in keyof TService]: `${TFeatureName}.${k extends string ? k : never}`;
 };
+
+export type Role = keyof typeof rolesPermissions;
+export type RolesPermissions = (typeof rolesPermissions)[Role];
+export type Permission = RolesPermissions extends Set<infer P> ? P : never;
