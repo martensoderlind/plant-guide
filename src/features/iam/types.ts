75a6a181c701b0d6c6c6e9956d4042dc23c35110
mårten söderlind <marten.soderlind@gmail.com>
2025-11-05 09:56:23 +0100

    setting up types for permissions

--- src/features/iam/types.ts
+++ src/features/iam/types.ts
@@ -45,3 +45,12 @@ export type IamService = {
   getUserCount: iamService["getUserCount"];
   updateUser: iamService["updateUser"];
 };
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Service = { [key: string]: (args: any) => Promise<any> };
+
+export type PermissionSchema<
+  TFeatureName extends string,
+  TService extends Service
+> = {
+  [k in keyof TService]: `${TFeatureName}.${k extends string ? k : never}`;
+};
