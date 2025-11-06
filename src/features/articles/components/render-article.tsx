79011306c3059fbd65f5f5f935e992a5bb5b4863
mårten söderlind <marten.soderlind@gmail.com>
2025-11-06 21:04:06 +0100

    refactor: configure StarterKit for improved list styling in RenderArticleContent

--- src/features/articles/components/render-article.tsx
+++ src/features/articles/components/render-article.tsx
@@ -5,7 +5,25 @@ import StarterKit from "@tiptap/starter-kit";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function RenderArticleContent({ content }: { content: any }) {
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit.configure({
+        bulletList: {
+          HTMLAttributes: {
+            class: "list-disc ml-4",
+          },
+        },
+        orderedList: {
+          HTMLAttributes: {
+            class: "list-decimal ml-4",
+          },
+        },
+        listItem: {
+          HTMLAttributes: {
+            class: "ml-4",
+          },
+        },
+      }),
+    ],
     immediatelyRender: false,
     content,
     editable: false,
