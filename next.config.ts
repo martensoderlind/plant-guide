a0615ea8bea6133816bfb7fd07a3c29561128429
mårten söderlind <marten.soderlind@gmail.com>
2025-11-06 13:06:48 +0100

    added missing remote image pattern for img.clerk.com

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.clerk.dev",
       },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
     ],
     formats: ["image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200],
