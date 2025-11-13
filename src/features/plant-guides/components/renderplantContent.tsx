aafd4b549087e65a8ef472ca78a343843a8d402b
mårten söderlind <marten.soderlind@gmail.com>
2025-11-13 22:27:57 +0100

    renamed component

---
+++ src/features/plant-guides/components/renderplantContent.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function RenderPlantContent({ content }: { content: any }) {
+  const editor = useEditor({
+    extensions: [
+      StarterKit.configure({
+        bulletList: {
+          HTMLAttributes: {
+            class: "list-disc ml-4",
+          },
+        },
+        orderedList: {
+          HTMLAttributes: {
+            class: "list-decimal ml-4",
+          },
+        },
+        listItem: {
+          HTMLAttributes: {
+            class: "ml-4",
+          },
+        },
+      }),
+    ],
+    immediatelyRender: false,
+    content,
+    editable: false,
+  });
+  return <EditorContent editor={editor} />;
+}
