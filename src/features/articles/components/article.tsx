04ca1c61b5b1a638e70d72f5ed4502892558d05f
mårten söderlind <marten.soderlind@gmail.com>
2025-11-17 17:00:43 +0100

    fallback component for article

--- src/features/articles/components/article.tsx
+++ src/features/articles/components/article.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import ArticleInfo from "./article-info";
 import { RenderContent } from "../../../components/render-content";
 import ArticleHeader from "./article-header";
+import ArticleFallback from "./article-fallback";
 
 type Prop = {
   slug: string;
@@ -13,13 +14,7 @@ export default async function Article({ slug }: Prop) {
   await articlesService.incrementArticleViews(slug);
   const article = await articlesService.getArticle(slug);
   if (!article) {
-    return (
-      <div className="max-w-4xl mx-auto py-8 flex flex-col justify-items-center">
-        <h1 className="text-xl md:text-2xl font-bold text-gray-200 mb-4 leading-tight mx-auto">
-          No article with that name exist.
-        </h1>
-      </div>
-    );
+    return <ArticleFallback />;
   }
 
   return (
