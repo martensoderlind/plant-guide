76ea9659f86cd79940b2eb2c422f3d9a560d48df
mårten söderlind <marten.soderlind@gmail.com>
2025-11-17 17:02:58 +0100

    updated to use Link from next/link

--- src/features/articles/components/article-fallback.tsx
+++ src/features/articles/components/article-fallback.tsx
@@ -1,13 +1,14 @@
-import { Link, Undo2 } from "lucide-react";
+import { Undo2 } from "lucide-react";
+import Link from "next/link";
 
 export default function ArticleFallback() {
   return (
     <div className="max-w-4xl mx-auto py-8 flex flex-col justify-items-center">
-      <h1 className="text-xl md:text-2xl font-bold text-gray-200 mb-4 leading-tight mx-auto">
+      <h1 className="text-xl md:text-2xl font-bold mb-4 leading-tight mx-auto">
         No article with that name exist.
       </h1>
       <Link href={"/articles"} className="mx-auto flex flex-row">
-        <span className="text-xl md:text-xl font-bold text-gray-200 mb-4 leading-tight mr-2">
+        <span className="text-xl md:text-xl font-bold mb-4 leading-tight mr-2">
           Return to article page
         </span>
         <Undo2 />
