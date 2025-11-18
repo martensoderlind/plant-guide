3af643d5c37581f1a348819f2ecd1080c9f697c4
mårten söderlind <marten.soderlind@gmail.com>
2025-11-18 11:39:17 +0100

    new db relations

--- src/features/user/schema.ts
+++ src/features/user/schema.ts
@@ -1,5 +1,6 @@
 import { relations } from "drizzle-orm";
 import { boolean, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { articleTable } from "../articles/schema";
 
 export const usersTable = pgTable("users", {
   id: text("id").primaryKey(),
@@ -36,3 +37,28 @@ export const authorProfilesTable = pgTable("author_profiles", {
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
+
+export const usersRelations = relations(usersTable, ({ one }) => ({
+  authorProfile: one(authorProfilesTable, {
+    fields: [usersTable.id],
+    references: [authorProfilesTable.userId],
+  }),
+}));
+
+export const authorProfilesRelations = relations(
+  authorProfilesTable,
+  ({ one, many }) => ({
+    user: one(usersTable, {
+      fields: [authorProfilesTable.userId],
+      references: [usersTable.id],
+    }),
+    articles: many(articleTable),
+  })
+);
+
+export const articlesRelations = relations(articleTable, ({ one }) => ({
+  author: one(authorProfilesTable, {
+    fields: [articleTable.author_id],
+    references: [authorProfilesTable.id],
+  }),
+}));
