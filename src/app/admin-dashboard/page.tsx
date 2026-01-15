b72d92aaa712aa3109c61b8e7f5b3a6c904fe642
mårten söderlind <marten.soderlind@gmail.com>
2026-01-15 10:38:49 +0100

    added force dynamic to admin-dashboard endpoint to make auth work

--- src/app/admin-dashboard/page.tsx
+++ src/app/admin-dashboard/page.tsx
@@ -1,5 +1,7 @@
 import AdminOverview from "@/features/admin-dashboard/components/admin-overview";
 
+export const dynamic = "force-dynamic";
+
 export default function Page() {
   return (
     <div>
