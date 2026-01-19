4db125f6532a8ee933c13b6d40c3f06b81c3afbe
mårten söderlind <marten.soderlind@gmail.com>
2026-01-19 11:47:42 +0100

    removed color from email

--- src/features/admin-dashboard/components/admin-users/admin-user-row-section.tsx
+++ src/features/admin-dashboard/components/admin-users/admin-user-row-section.tsx
@@ -35,7 +35,7 @@ export default function AdminUserRowSection({
           <div className="text-sm font-medium text-gray-900">
             {fullname || username}
           </div>
-          <span className="inline-flex items-center px-2 py-0.5 rounded text-xs font-medium bg-blue-100 text-blue-800">
+          <span className="inline-flex items-center px-2 py-0.5 rounded text-xs font-medium bg-gray-100 text-gray-600">
             {email}
           </span>
         </div>
